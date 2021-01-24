baf9294f38fa16d938acd413b52578206cf7bcfb
tdstyrone <tdstyrone@gmail.com>
2021-01-24 15:10:17 -0400

    Initial Commit

---
+++ large-scale-programming/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ Name: Tyrone Smith
+ StudentID: @02943142
+ Class: Large Scale Programming
+ Assignment: 1
+*/
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+	}
+
+}
